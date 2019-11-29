f05e9e392664180a80c99e86d9ea782898c46f5d
Augusto Borges <gtoborges@gmail.com>
2019-11-29 01:50:50 -0300

    fix tables e models com relacionamento errado, remove tabelas desnecessarias

--- server/scripts/testes.js
+++ server/scripts/testes.js
@@ -3,7 +3,7 @@ const sequelize = db.sequelize
 const Sequelize = require('sequelize')
 const Op = Sequelize.Op
 
-async function executar() {
+async function testeJogador() {
 
   let opcoes = {
     include: [
@@ -20,15 +20,38 @@ async function executar() {
 
   let a = await db.jogador.findAll(opcoes)
   console.log(a[0].dataValues)
-
   console.log('\n\n\n')
-
   console.log(a[0].equipe.dataValues)
-
   console.log('\n\n\n')
-
   console.log(a[0].get('paisDeOrigem').dataValues)
+  console.log('\n\n\n')
+  console.log(a.length)
 
 }
 
-executar()
+// testeJogador()
+
+async function testeEquipe() {
+
+  let opcoes = {
+    include: [
+      {
+        model: db.paises,
+        as: 'paisDeOrigem'
+      },
+    ],
+  }
+
+  let a = await db.equipe.findAll(opcoes)
+  console.log(a[0].dataValues)
+  console.log('\n\n\n')
+  console.log(a[1].dataValues)
+  console.log('\n\n\n')
+  console.log(a.length)
+
+  let b = await db.jogador.findAll({where: {idEquipe: a[0].idEquipe}, raw: true})
+  console.log(b)
+
+}
+
+testeEquipe()
