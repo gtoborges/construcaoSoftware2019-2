5dd2b67a9cb8cfee107d6e1d80affea8ff61f517
Augusto Borges <gtoborges@gmail.com>
2019-11-30 13:28:31 -0300

    ajustando scripts de testes no backend e refactor no front end

--- server/scripts/buscarJogadoresEEquipes.js
+++ server/scripts/buscarJogadoresEEquipes.js
@@ -47,7 +47,7 @@ async function testeEquipe() {
   }
 
 }
-testeEquipe()
+// testeEquipe()
 
 async function jogadoresDaEquipe() {
 
@@ -84,31 +84,4 @@ async function jogadoresDaEquipe() {
   }
 
 }
-jogadoresDaEquipe()
-
-async function criarEquipesEJogadores(){
-
-  const equipes = require("./equipes.js").equipes
-  const jogadores = require("./jogadores.js").jogadores
-
-  try {
-    let transacao = await db.sequelize.transaction({ isolationLevel: db.Sequelize.Transaction.ISOLATION_LEVELS.READ_UNCOMMITTED })
-    
-    for(equipe of equipes) {
-      await db.equipe.create(equipe, { transaction: transacao })
-    }
-
-    for(jogador of jogadores) {
-      await db.jogador.create(jogador, { transaction: transacao })
-    }
-
-    await transacao.commit();
-    // await transacao.rollback();
-  } catch (e) {
-    console.error(e)
-    await transacao.rollback();
-    throw e
-  }
-  
-}
-criarEquipesEJogadores()
+// jogadoresDaEquipe()
---
+++ server/scripts/criarEquipesEjogadores.js
@@ -0,0 +1,32 @@
+const db = require('../app/models')
+const sequelize = db.sequelize
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
+
+async function criarEquipesEJogadores(){
+
+  const equipes = require("./equipesSeed.js/index.js").equipes
+  const jogadores = require("./jogadoresSeed.js").jogadores
+
+  try {
+    let transacao = await db.sequelize.transaction({ isolationLevel: db.Sequelize.Transaction.ISOLATION_LEVELS.READ_UNCOMMITTED })
+    
+    for(equipe of equipes) {
+      await db.equipe.create(equipe, { transaction: transacao })
+    }
+
+    for(jogador of jogadores) {
+      await db.jogador.create(jogador, { transaction: transacao })
+    }
+
+    await transacao.commit();
+    // await transacao.rollback();
+  } catch (e) {
+    console.error(e)
+    await transacao.rollback();
+    throw e
+  }
+  
+}
+
+criarEquipesEJogadores()
